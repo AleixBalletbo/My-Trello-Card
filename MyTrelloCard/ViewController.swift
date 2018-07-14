f4d8dcc25986d378b1736b0d4d556481d071f5c2
Aleix Balletbó <aleix.balletbo@est.fib.upc.edu>
2018-07-15 00:03:17 +0200

    Initial commit

---
+++ MyTrelloCard/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyTrelloCard
+//
+//  Created by Aleix Balletbó on 15/7/18.
+//  Copyright © 2018 Aleix Balletbó. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
